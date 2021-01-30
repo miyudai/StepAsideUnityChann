b56a3df71e0716780a78d860c59b5e5053fc85b9
miyudai <49362030+miyudai@users.noreply.github.com>
2021-01-30 20:54:40 +0900

    Initial commit

---
+++ Assets/ItemDestroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class ItemDestroy : MonoBehaviour
+{
+
+    // アイテム情報
+    private ItemGenerator Item;
+    // カメラの距離
+    private float difference;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        Camera camera = Camera.main;
+
+        if (camera.transform.position.z> gameObject.transform.position.z)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
